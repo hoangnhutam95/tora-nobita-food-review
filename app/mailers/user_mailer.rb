1b26ea30e198300ef59019dff0d770a5772d5abd
duc2495 <vuongtrungduong1995@gmail.com>
2017-10-11 23:18:22 +0700

    Resend mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,6 +9,12 @@ class UserMailer < ApplicationMailer
     @user = user
     mail to: user.email, subject: "Account activation"
   end
+  
+  def resend_activation(user)
+    @user = user
+    @userid = user.id
+    mail to: user.email, subject: "Account activation"
+  end
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
     resources :users
     resources :account_activations, only: [:edit]
     resources :password_resets,     only: [:new, :create, :edit, :update]
+    get   '/resend_activation/:email', to: 'account_activations#resend_activation', :constraints => { :email => /[^\/]+/ }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
